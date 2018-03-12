1b4f45cd3595fd95da5aa226c058cb72b9433593
Richard <richard@fickert.nl>
2018-03-12 16:39:32 +0100

    Create plugin.py

---
+++ plugin.py
@@ -0,0 +1,14 @@
+# NeoPixel Status Plugin
+#
+# Author: Richard Fickert
+#
+"""
+<plugin key="NeoPixelStatus" name="NeoPixel status notifier" author="Richard Fickert" version="0.1" wikilink="https://github.com/gerard33/sonos">
+    <params>
+        <param field="LED_COUNT" label="LED count" width="25px" required="true" default="16"/>
+        <param field="LED_PIN" label="GPIO pin" width="25px" required="true" default="18"/>
+        <param field="LED_BRIGHTDAY" label="Brightness at daytime" width="25px" required="true"  default="255"/>
+        <param field="LED_BRIGHTNIGHT" label="Brightness at night" width="25px" required="true"  default="127"/>
+    </params>
+</plugin>
+"""
